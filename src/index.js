7f00d309da05e344a53e06cac8c03b7209c20e88
yangyi <yhxyangyi@163.com>
2019-02-20 17:37:02 +0800

    ts-add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import GlobalLayout from '@/layout/globalLayout'
 import router from '@/router'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import '@public/assets/styles/antd.less'
 import '@public/assets/styles/index.scss'
 import store from './store/index'
 import '@babel/polyfill'
 import Viser from 'viser-vue' //图表库引入
 
-Vue.use(Antd); //使用Antd
 
 Vue.use(Viser);
 Vue.config.productionTip = false
